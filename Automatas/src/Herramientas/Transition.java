f8df5bcc3ccdc4040362e6402371a7fef859c847
Fabian Montes <fmontes@unal.edu.co>
2020-05-16 20:16:58 -0500

    ads

--- Automatas/src/Herramientas/Transition.java
+++ Automatas/src/Herramientas/Transition.java
@@ -5,27 +5,30 @@
  */
 package Herramientas;
 
-import Herramientas.Tuple;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- *  clase 
- * @author fanat
+ *  La clase Transition sirve para simular transiciones de  automatas tipo AFD
+ *  posee un hashmap para cada simbolo del alphabeto que guarda las transiciones de cada estado a siguiente estado 
  */
 public class Transition {
     HashMap<Character,HashMap<String,String>> transicion;
 
+    /**
+     * crea el hash para guardar las transiciones 
+     */
+    
     public Transition() {
         transicion = new HashMap<>();
     }
     
     /**
-     *
-     * @param Estado
-     * @param Alphabeto
-     * @param estado
+     *  añade la trancision para determinado estado
+     * @param Estado Estado actual
+     * @param Alphabeto caracter que genera la transicion
+     * @param estado estado al que se realizara la transicion
      */
+    
     public void add(Character Alphabeto, String Estado,String estado){
         if(transicion.get(Alphabeto)==null){
             transicion.put(Alphabeto, new HashMap<>());
@@ -33,11 +36,23 @@ public class Transition {
         transicion.get(Alphabeto).put(Estado, estado);
     }
     
+    /**
+     * cantidad de caracteres en el alfabeto
+     * @return 
+     */
+    
 
     public int size() {
         return transicion.size();
     }
 
+    /**
+     * Realiza la trancision de un estado segun el caracter recibido
+     * @param Alphabeto caracter que marca el donde de la transicion
+     * @param Estado estado desde donde se realiza la transicion
+     * @return estado al que se debe desplazar
+     */
+    
     public String cambio(Character Alphabeto, String Estado) {
         return transicion.get(Alphabeto).get(Estado);
     }
