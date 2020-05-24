6a27f2b5943ec42d6c4df50560b74468308bcd6f
jdacostabe <jdacostabe@unal.edu.co>
2020-05-24 16:14:35 -0500

    Javadoc Añadido

--- Automatas/src/ProcesamientoCadenas/ProcesamientoCadenaAFN.java
+++ Automatas/src/ProcesamientoCadenas/ProcesamientoCadenaAFN.java
@@ -4,24 +4,44 @@ import Herramientas.NDRespuesta;
 import java.util.ArrayList;
 
 /**
- *
+ * Esta clase se utiliza para la impresión de los diferentes procesamientos de una cadena en un automata
  * @author jdacostabe
  */
 public class ProcesamientoCadenaAFN {
+    /**
+     * El atributo cadena se utiliza para guardar la cadena a la cual pertenecen los procesamientos que se imprimirán.
+     */
     String cadena;
+    /**
+     * El atributo isAccepted se utiliza para indicar si la cadena es aceptada o no.
+     */
     boolean isAccepted;
+    /**
+     * El atributo procesamientos es un tipo de dato que guarda todos los procesamientos de una cadena (Aceptados, rechazados o abortados).
+     */
     NDRespuesta procesamientos;
     
+    /**
+     * Constructor, inicializa los atributos.
+     * @param cadena Cadena a la cual se le realizó el procesamiento en el automata
+     * @param procesamientos Lista de procesamientos aceptados, rechazados y abortados
+     */
     public ProcesamientoCadenaAFN(String cadena, NDRespuesta procesamientos){
         this.cadena = cadena;
         this.isAccepted = procesamientos.isAccepted();
         this.procesamientos = procesamientos;
     }
     
+    /**
+     * Imprime si la cadena es aceptada o no.
+     */
     public void procesarCadena(){
         System.out.println("Procesar cadena: "+ isAccepted+"\n");
     }
     
+    /**
+     * Imprime un solo procesamiento de la cadena dependiendo de si es aceptado, rechazado o no tiene procesamientos terminados
+     */
     public void procesarCadenaConDetalles(){
         System.out.println("Procesar cadena con detalles:");
         if(procesamientos.isAccepted()){
@@ -34,6 +54,9 @@ public class ProcesamientoCadenaAFN {
         System.out.println("\n");
     }
     
+    /**
+     * Imprime todos los procesamiento de la cadena (Aceptados, rechazados y abortados)
+     */
     public void computarTodosLosProcesamientos(){
         System.out.println("Computar todos los procesamientos:");
         String res="Aceptadas:\n  ";
@@ -60,6 +83,9 @@ public class ProcesamientoCadenaAFN {
         System.out.println(res);
     }
     
+    /**
+     * Realiza todos los tipos de impresiones de los procesamientos de la cadena.
+     */
     public void imprimirRespuestas(){
         System.out.println("Cadena: "+this.cadena+"\n");
         procesarCadena();
