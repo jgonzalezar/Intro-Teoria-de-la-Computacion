4a63adfec235e257486f357fe107ce90cad691cd
Juan Andrés Gonzalez Arias <jgonzalezar@unal.edu.co>
2020-05-27 17:39:02 -0500

    Lo que va del afnl

--- Automatas/src/AutomatasFinitos/AFNL.java
+++ Automatas/src/AutomatasFinitos/AFNL.java
@@ -5,39 +5,167 @@
  */
 package AutomatasFinitos;
 import Herramientas.TransitionAFNL;
+import Herramientas.Tuple;
+import LectoresYProcesos.CreadorAutomata;
+import java.io.File;
+import java.io.FileNotFoundException;
+import static java.lang.Integer.parseInt;
 import java.util.Map;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author ivonn
  */
 public class AFNL {
     private final char[] E;
-    private final int Q;
+    private final ArrayList<String> Q;
     private final int q0;
-    private final int[] F;
+    private final ArrayList<Integer> F;
+    //public final ArrayList<ArrayList<Tuple>> Delta;
     private final TransitionAFNL T;
     private final char lambda;
     
 
-    public AFNL(char[] E, int Q, int q0, int[] F, TransitionAFNL T) {
+    public AFNL(char[] E, ArrayList<String> Q, int q0, ArrayList<String> F, TransitionAFNL T) {
         this.E = E;
         this.Q = Q;
         this.q0 = q0;
-        this.F = F;
+        this.F = new ArrayList<Integer>();
+        for(int i=0;i<F.size();i++)
+            this.F.add(Q.indexOf(F.get(i)));
         this.T = T;
-        this.lambda = 'L';
+        this.lambda = '$';
     }
     
-    public AFNL(char[] E, int Q, int q0, int[] F, TransitionAFNL T,char lambda) {
+    public AFNL(char[] E, ArrayList<String> Q, int q0, ArrayList<String> F, TransitionAFNL T,char lambda) {
         this.E = E;
         this.Q = Q;
         this.q0 = q0;
-        this.F = F;
+        this.F = new ArrayList<Integer>();
+        for(int i=0;i<F.size();i++)
+            this.F.add(Q.indexOf(F.get(i)));
         this.T = T;
         this.lambda = lambda;
+    }
+    
+    public AFNL(String nombreArchivo) {
+        CreadorAutomata.Lecto lec = CreadorAutomata.Lecto.inicio;
+        ArrayList<Character> alpha = null;
+        ArrayList<String> Estados = null;
+        String q0 = null;
+        ArrayList<String> F = null;
+        //ArrayList<Tuple> Delta = null;
+        TransitionAFNL transition = null;
+        
+        
+        try {
+            Scanner sca = new Scanner(new File(nombreArchivo));
+            while(sca.hasNextLine()){
+                String lin = sca.nextLine();
+                System.out.println(lin);
+                switch(lin){
+                    case "#alphabet":
+                        lec = CreadorAutomata.Lecto.alfabeto;
+                        alpha = new ArrayList<>();
+                        break;
+                    case "#states":
+                        if(alpha==null) throw new Error("primero debe iniciarse el alfabeto");
+                        lec = CreadorAutomata.Lecto.estados;
+                        Estados = new ArrayList<>();
+                        break;
+                    case "#initial":
+                        if(alpha==null) throw new Error("primero debe iniciarse el alfabeto");
+                        if(Estados==null) throw new Error("primero debe iniciarse los estados");
+                        lec = CreadorAutomata.Lecto.estadoinicial;
+                        break;
+                    case "#accepting":
+                        if(alpha==null) throw new Error("primero debe iniciarse el alfabeto");
+                        if(Estados==null) throw new Error("primero debe iniciarse los estados");
+                        if(q0==null) throw new Error("primero debe dar el estado inicial");
+                        lec = CreadorAutomata.Lecto.estadoFin;
+                        F = new ArrayList<>();
+                        break;
+                    case "#transitions":
+                        if(alpha==null) throw new Error("primero debe iniciarse el alfabeto");
+                        if(Estados==null) throw new Error("primero debe iniciarse los estados");
+                        if(q0==null) throw new Error("primero debe dar el estado inicial");
+                        if(F==null) throw new Error("primero debe dar los estados finales");
+                        lec = CreadorAutomata.Lecto.transicion;
+                        transition = new TransitionAFNL(Estados.size());
+                        break;
+                    default:
+                        switch(lec){
+                            case alfabeto:
+                                if(lin.length()<2){
+                                    alpha.add(lin.charAt(0));
+                                }else{
+                                    int a = lin.charAt(0);
+                                    int b = lin.charAt(2);
+                                    int c = b-a;
+                                    for (int i = 0; i <= c; i++) {
+                                        char d = (char) (a+i);
+                                        alpha.add(d);
+                                        
+                                    }
+                                }
+                                break;
+                            case estados:
+                                Estados.add(lin);
+                                break;
+                            case estadoinicial:
+                                if(!Estados.contains(lin))throw new Error("el estado debe pertenecer a los estados del automata");
+                                q0=lin;
+                                break;
+                            case estadoFin:
+                                if(!Estados.contains(lin))throw new Error("el estado debe pertenecer a los estados del automata");
+                                F.add(lin);
+                                break;
+                            case transicion:
+                                
+                                String[] origin = lin.split(":");
+                                String estado1 = origin[0];
+                                if(!Estados.contains(estado1))throw new Error("el estado del que se parte debe pertenecer a los estados del automata");
+                                String[] origin2 = origin[1].split(">");
+                                String alpfa = origin2[0];
+                                if(!alpha.contains(alpfa.charAt(0)) && !alpfa.equals("$"))throw new Error("el caracter de activacion debe pertenecer al alfabeto");
+                                String[] origin3 = origin2[1].split(",");
+                                int[] destino = new int[origin3.length];
+                                for(int i=0;i<origin3.length;i++){
+                                    String estado2 = origin3[i];
+                                    if(!Estados.contains(estado2))throw new Error("el estado de llegada debe pertenecer a los estados del automata");
+                                    //Delta.add(new Tuple(alpfa.substring(0), parseInt(estado1), parseInt(estado2)));
+                                    destino[i] = Estados.indexOf(estado2);
+                                }
+                                transition.add(alpfa.charAt(0), Estados.indexOf(estado1), destino);
+                                break;
+                            default:
+                                break;
+                        }
+
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(CreadorAutomata.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        char[] ad =new char[alpha.size()];
+        for (int i = 0; i < alpha.size(); i++) {
+            ad[i]=alpha.get(i);
+        }
+        this.E = ad;
+        this.Q = Estados;
+        this.q0 = Q.indexOf(q0);
+        this.F = new ArrayList<Integer>();
+        for(int i=0;i<F.size();i++)
+            this.F.add(Q.indexOf(F.get(i)));
+        this.T = transition;
+        this.lambda = '$';
+        
     }
     
     //@SuppressWarnings("empty-statement")
--- Automatas/src/Herramientas/TransitionAFNL.java
+++ Automatas/src/Herramientas/TransitionAFNL.java
@@ -10,10 +10,22 @@ import java.util.HashMap;
  * @author ivonn
  */
 public class TransitionAFNL {
-    ArrayList<HashMap<Character,ArrayList<Integer>>> transicionAFNL;
+        ArrayList<HashMap<Character,ArrayList<Integer>>> transicionAFNL;
     
-    public TransitionAFNL() {
+    public TransitionAFNL(int indice) {
         transicionAFNL = new ArrayList<>();
+        transicionAFNL.ensureCapacity(indice);
+    }
+    
+    public void add(Character Alphabeto, int Estado, int... estado){
+        if(transicionAFNL.get(Estado)==null){
+            transicionAFNL.set(Estado, new HashMap<>());
+        }
+        ArrayList<Integer> state = new ArrayList<>();
+        for(int i  = 0; i < estado.length; i++){
+            state.add(estado[i]);
+        }
+        transicionAFNL.get(Estado).put(Alphabeto, state);
     }
     
      public void Add(HashMap letter_states){
