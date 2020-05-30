b4e397bd7f1aa370cb59faa96a321a353c2a3f29
Fabian Montes <fmontes@unal.edu.co>
2020-05-30 13:44:05 -0500

    asd

---
+++ Automatas/src/LectoresYProcesos/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * El paquete Lectores Y Procesos contiene la clase main junto con una clase con los enum y algunas funciones estaticas usadas en otras clases
+ */
+package LectoresYProcesos;
---
+++ Automatas/src/ProcesamientoCadenas/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * El paquete creador contiene un mini proyecto capaz de crear un archivo que contenga todos el Sigma*
+ * de un alphabeto hasta cierta cantidad de digitos dados por el usuario
+ */
+package Creador;
