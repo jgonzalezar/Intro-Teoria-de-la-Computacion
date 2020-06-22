17ad79beec1389e59dcac7e4851dfd3e5c147e0e
brandon <brandon@brandon-Lenovo-G400>
2020-06-21 22:26:10 -0500

    Organización de paquetes productoAFD

--- Automatas/src/AutomatasFinitos/AFDHerramientasProducto/ElegirTipoProductoAFD.java
+++ Automatas/src/AutomatasFinitos/AFDHerramientasProducto/ElegirTipoProductoAFD.java
@@ -5,10 +5,41 @@
  */
 package AutomatasFinitos.AFDHerramientasProducto;
 
+import AutomatasFinitos.AFD;
+import java.util.ArrayList;
+
 /**
  *
  * @author brandon
  */
-public class ElegirTipoProductoAFD {
+public class ElegirTipoProductoAFD extends AFDProducto{
     
+    public ElegirTipoProductoAFD(AFD afd1,AFD afd2, String operacion){
+        super(afd1, afd2);
+        F = new ArrayList<>();
+        String type = operacion;
+        switch (type)
+        {
+            case "union":
+                AFDProductoO estadosUnion;
+                estadosUnion = new AFDProductoO(afd1, afd2);
+                F = estadosUnion.getF();
+                break;
+            case "interseccion":
+                AFDProductoY estadosInter;
+                estadosInter = new AFDProductoY(afd1, afd2);
+                F = estadosInter.getF();
+                break;
+            case "diferencia":
+                AFDProductoD estadosDif;
+                estadosDif = new AFDProductoD(afd1, afd2);
+                F = estadosDif.getF();
+                break;
+            case "diferenciaSimetrica":
+                AFDProductoDSim estadosDSim;
+                estadosDSim = new AFDProductoDSim(afd1, afd2);
+                F = estadosDSim.getF();
+                break;
+        }
+    } 
 }
