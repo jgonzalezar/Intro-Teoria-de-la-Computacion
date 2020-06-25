320cbb2417ecdb57cc5bf8f29c0872a2b036a621
Fabian Montes <fmontes@unal.edu.co>
2020-06-24 21:07:32 -0500

    asd

--- Automatas/src/LectoresYProcesos/ClaseValidacion.java
+++ Automatas/src/LectoresYProcesos/ClaseValidacion.java
@@ -16,44 +16,13 @@ import java.util.ArrayList;
  * @author fanat
  */
 public class ClaseValidacion {
-    private AFNL afnl;
-    private AFN afn;
-    
-    /**
-     * el constructor que recibe un AFNL y un afn
-     * @param afnl AFNL base
-     * @param afn AFN base
-     */
-    
-    public ClaseValidacion(AFNL afnl, AFN afn) {
-        this.afnl = afnl;
-        this.afn = afn;
-    }
-    /**
-     * el constructor que recibe  un afn
-     * @param afn AFN base
-     */
-    
-    public ClaseValidacion(AFN afn) {
-        this.afn = afn;
-    }
-    
-    /**
-     * el constructor que recibe un AFNL
-     * @param afnl AFNL base
-     */
-    
-    public ClaseValidacion(AFNL afnl) {
-        this.afnl = afnl;
-    }
-    
     /**
      * metodo que valida por medio de la comparacion de cadenas aleatorias de un alfabeto sobre el AFNL y su tranformacion
      * @param sigma alfabeto a utilizar
      */
     
-    public void validarAFNLambdaToAFN(Alfabeto sigma){
-        AFN Afn = afnl.AFN_LambdaToAFN();
+    public void validarAFNLambdaToAFN(Alfabeto sigma,AFN listaDeAFDs[]){
+       /* AFN Afn = afnl.AFN_LambdaToAFN();
         int cantidadrechazados = 0;
         ArrayList <String> rechazadas = new ArrayList<>();
         for (int i = 0; i < 5000; i++) {
@@ -65,14 +34,14 @@ public class ClaseValidacion {
         }
         System.out.println("Casos con el mismo resultado: " + (5000-cantidadrechazados));
         System.out.println("Casos con diferente resultado: " + cantidadrechazados);
-        System.out.println(rechazadas.toString().substring(1, rechazadas.toString().length()-1));
+        System.out.println(rechazadas.toString().substring(1, rechazadas.toString().length()-1));*/
     }
     /**
      * metodo que valida por medio de la comparacion de cadenas aleatorias de un alfabeto sobre el AFN y su tranformacion
      * @param sigma alfabeto a utilizar
      */
-    public void validarAFNtoAFD(Alfabeto sigma){
-        AFD Afd = afn.AFNtoAFD();
+    public void validarAFNtoAFD(Alfabeto sigma,AFN listaDeAFNs[]){
+        /*AFD Afd = afn.AFNtoAFD();
         int cantidadrechazados = 0;
         ArrayList <String> rechazadas = new ArrayList<>();
         for (int i = 0; i < 5000; i++) {
@@ -84,7 +53,7 @@ public class ClaseValidacion {
         }
         System.out.println("Casos con el mismo resultado: " + (5000-cantidadrechazados));
         System.out.println("Casos con diferente resultado: " + cantidadrechazados);
-        System.out.println(rechazadas.toString().substring(1, rechazadas.toString().length()-1));
+        System.out.println(rechazadas.toString().substring(1, rechazadas.toString().length()-1));*/
     }
     
 }
